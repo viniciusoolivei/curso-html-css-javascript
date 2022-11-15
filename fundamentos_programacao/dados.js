7d58a65f02f3a80004c13339bfaf4377a7218fab
viniciusoolivei <viniciusolivei@gmail.com>
2022-11-15 19:19:48 -0300

    dados js

---
+++ fundamentos_programacao/dados.js
@@ -0,0 +1,11 @@
+var nome = 'caneta';
+var quantidade = 10;
+let preco = 6.4;//let é a forma mais usual de declarar variavel em js
+let imposto = 1.5;
+let precoFinal = preco+imposto;//variavel com duas palavras não devem ser toda escrita em letras minusculas
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log('Preço final',precoFinal);
